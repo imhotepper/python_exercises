4bf61c4414510a03281752b92f443a8e4cf4f212
Dragosh <176727+imhotepper@users.noreply.github.com>
2023-11-10 19:05:23 +0000

    spaces removed

--- mode.py
+++ mode.py
@@ -15,9 +15,7 @@ def mode(numbers:list):
 
         
 assert mode([]) == None
-
 assert mode([1, 2, 3, 4, 4]) == 4
-
 assert mode([1, 1, 2, 3, 4]) == 1
 
 import random
@@ -25,9 +23,6 @@ import random
 random.seed(42)
 
 testData = [1, 2, 3, 4, 4]
-
 for i in range(1000):
-
     random.shuffle(testData)
-
     assert mode(testData) == 4
